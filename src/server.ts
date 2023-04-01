c68c007678ccd30f4edf09a3a0426eb97652bb68
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2023-04-01 18:15:21 -0300

    feat: implemented authentication routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import fastify from "fastify";
+import { Authentication } from "./routes/authentication";
 
 const app = fastify();
+
+app.register(Authentication, {
+  prefix: "authentication",
+});
 
 app.listen({ port: 3556 }).then(() => {
   console.info("Server is running");
