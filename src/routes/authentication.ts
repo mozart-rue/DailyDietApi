9f03dcb0166808a0c54e26448b179713d10e5d6d
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2023-04-01 21:29:08 -0300

    feat: route signup, save user on database

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { generateSalt, hashPassword } from "../lib/encrypter";
 
 export async function Authentication(app: FastifyInstance) {
-  app.post("/signup", (req, res) => {
+  app.post("/signup", async (req, res) => {
     const createNewAccountBodySchema = z.object({
       email: z.string(),
       name: z.string(),
@@ -12,6 +14,32 @@ export async function Authentication(app: FastifyInstance) {
     const { email, name, password } = createNewAccountBodySchema.parse(
       req.body
     );
+
+    // Check if the e-mail already exists
+    const userEmail = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (userEmail) {
+      return res.status(400).send({
+        error: "E-mail already registered",
+      });
+    }
+
+    // Encrypt user password
+    const salt = await generateSalt();
+    const hashedPassword = await hashPassword(password, salt);
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password_salt: salt,
+        password_hash: hashedPassword,
+      },
+    });
 
     return res.status(201).send();
   });
