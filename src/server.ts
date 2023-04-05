0172206677273299a707b090b5b87cc25d6fd6f7
Mozart Rueckert <suporte4@grupoinovasse.com.br>
2023-04-04 21:45:45 -0300

    feat: add meals routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import fastify from "fastify";
 import { Authentication } from "./routes/authentication";
+import { Meals } from "./routes/meals";
 
 const app = fastify();
 
 app.register(Authentication, {
   prefix: "authentication",
+});
+
+app.register(Meals, {
+  prefix: "meals",
 });
 
 app.listen({ port: 3556 }).then(() => {
